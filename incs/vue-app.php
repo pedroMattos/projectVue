fee187747aee672ea5456d29423ca0e4df38abbf
Pedro Mattos <laranjeiraspedro@gmail.com>
2019-11-26 19:09:24 -0300

    add vue app

---
+++ incs/vue-app.php
@@ -0,0 +1,13 @@
+<script>
+    // var app = new Vue({
+    //     el: "#app",
+    //     data: {
+    //         titulo: "Titulo de teste",
+    //         lang: [
+    //             {nome: "Pedro"},
+    //             {nome: "teste"},
+    //             {nome: "teste2"},
+    //         ]
+    //     }
+    // });
+</script>
